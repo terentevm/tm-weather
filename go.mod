966f91eefbe3c092f98c24adf3c6555c28b2990f
terentevm <mick911@mail.ru>
2019-12-11 11:00:09 +0100

    added suppor .env files

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/terentevm/tm-weather
+module github.com/terentev/tm-weather
 
 go 1.13
+
+require github.com/joho/godotenv v1.3.0
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -18,7 +19,12 @@ func (api apiData) getAPIParam() string {
 	return api.ParamName + "=" + api.Key
 }
 
-func serveReverseProxy(target string, apiKey apiData, res http.ResponseWriter, req *http.Request) {
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+}
+
+func serveWeatherstackSP(target string, apiKey apiData, res http.ResponseWriter, req *http.Request) {
 	// parse the url
 	url, _ := url.Parse(target)
 
@@ -35,26 +41,57 @@ func serveReverseProxy(target string, apiKey apiData, res http.ResponseWriter, r
 	req.URL.Path = "autocomplete"
 	req.URL.RawQuery = targetQuery
 
-	fmt.Println(req.URL.String())
-	fmt.Println(req.URL.Path)
-	fmt.Println(req.URL.RawQuery)
+	proxy.ServeHTTP(res, req)
+}
 
-	// res.Header().Set("Access-Control-Allow-Origin", "*")
-	// res.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS")
-	// Note that ServeHttp is non blocking and uses a go routine under the hood
+func serveYandexWeatherSP(target string, apiKey string, res http.ResponseWriter, req *http.Request) {
+	// parse the url
+	url, _ := url.Parse(target)
+
+	// create the reverse proxy
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	// Update the headers to allow for SSL redirection
+	req.URL.Host = url.Host
+	req.URL.Scheme = url.Scheme
+
+	req.Host = url.Host
+	req.URL.Path = "v1/forecast"
+
+	req.Header.Set("X-Yandex-API-Key", apiKey)
+
+	enableCors(&res)
+
 	proxy.ServeHTTP(res, req)
 }
 
 func handleAutocomplete(res http.ResponseWriter, req *http.Request) {
+
 	targetURL := "https://api.weatherstack.com"
 
-	keyData := apiData{ParamName: "access_key", Key: "00a232561814c1aa1784c70836d537d9"}
+	apixuAPIKEey := os.Getenv("WEATHERSTACK_API_KEY")
 
-	serveReverseProxy(targetURL, keyData, res, req)
+	keyData := apiData{ParamName: "access_key", Key: apixuAPIKEey}
+
+	serveWeatherstackSP(targetURL, keyData, res, req)
+
+}
+
+func handleForecast(res http.ResponseWriter, req *http.Request) {
+	targetURL := "https://api.weather.yandex.ru"
+
+	yaAPIKEey := os.Getenv("YA_WEATHER_API_KEY")
+
+	serveYandexWeatherSP(targetURL, yaAPIKEey, res, req)
 
 }
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		query, ok := req.URL.Query()["query"]
@@ -65,13 +102,14 @@ func main() {
 	})
 
 	port := os.Getenv("PORT")
+
 	if port == "" {
 		port = "9000"
 	}
 	addr := ":" + port
 	log.Printf("Listening on %s...\n", addr)
 	http.HandleFunc("/api/autocomplete/", handleAutocomplete)
-
+	http.HandleFunc("/api/forecast/", handleForecast)
 	http.ListenAndServe(addr, nil)
 
 }
